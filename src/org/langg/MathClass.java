4dfb80bb2943526e2a1bd45b9909f4990ed4b3a0
ganeshtvm01 <See>
2022-08-29 12:36:24 +0530

    ClassAndMethodsInJava

---
+++ src/org/langg/MathClass.java
@@ -0,0 +1,10 @@
+package org.langg;
+
+public class MathClass {
+	public static void main(String[] args) {
+		//here in math class the method is static
+		double sqrt = Math.sqrt(4);
+		System.out.println(sqrt);
+	}
+
+}
